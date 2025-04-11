e9ddcca05b0e093156fa84d055f9432a90d43bbc
AmitKumar <just4uamitkumar@gmail.com>
2025-04-11 20:46:16 +0530

    added login api

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import path from "path";
 import cors from 'cors';
 import { connectDB } from "./config/db.js";
+import cookieParser from 'cookie-parser';
 
 import productRoutes from "./routes/product.route.js";
 import jyotirlingRoutes from "./routes/jyotirling.route.js";
@@ -12,12 +13,17 @@ import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
+
+
+
+
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/products", productRoutes);
 app.use("/api/jyotirlings", jyotirlingRoutes);
@@ -30,6 +36,8 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
+
+
 
 app.listen(PORT, () => {
 	connectDB();
